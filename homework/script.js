f33dfefb5d525961a2dbfa59111dc9ddc87b1aeb
Edward Dudas <edwarddudas@gmail.com>
2024-02-01 13:12:00 +0200

    the homework is final

---
+++ homework/script.js
@@ -0,0 +1,20 @@
+let age = 13;
+console.log(age);
+let string = 'Edward';
+console.log(string);
+const isStudent = true;
+console.log(isStudent);
+const myString = 'Поки ти тримаєшся за свою «стабільність», хтось поряд втілює твої мрії. Роберт Орбен';
+console.log(myString);
+let myNumber = (100);
+console.log(myNumber+10);
+const myNull = null;
+console.log(myNull);
+
+const message = prompt('Enter your name');
+console.log(message);
+
+const thanks = confirm('Чи вам є 18 років?');
+
+const danger = alert('Увага! Ця дія може сприймати небезпечні файли!');
+const yesOrNo = confirm('Будь ласка надайте нам доступ, щоб ми могли видалити небезпечні фаули');
